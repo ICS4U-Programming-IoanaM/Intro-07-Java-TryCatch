5f69f7c490179917badc81f1663af2180b2692ca
Ioana Marinescu <ioana.marinescu@stu.ocsb.ca>
2024-02-21 03:10:18 +0000

    completed Intro-07 problem with try catch

--- TryCatch.java
+++ TryCatch.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+
 /**
  * Calculates volume of a sphere, with try catch.
  *
@@ -6,33 +7,38 @@ import java.util.Scanner;
  * @version 1.0
  * @since 2024-02-20
  */
-
-public final class VolSphere {
-  /**
-   * Private constructor to prevent instantiation of this utility class.
-   */
-  private VolSphere() {
-  }
+public final class TryCatch {
+  /** Private constructor to prevent instantiation of this utility class. */
+  private TryCatch() { }
 
   /**
    * The main method calculates volume of a sphere.
    *
-   * @param args Command-line arguments (not used in this program)
+   * @param args Command-line arguments (not used in this program).
    */
   public static void main(final String[] args) {
-    // import scanner and getting user radius from user
+    // import scanner and getting user radius from user.
     final Scanner sc = new Scanner(System.in);
     System.out.println("Enter radius (cm): ");
-    final double radius = sc.nextDouble();
+    final String stringRadius = sc.nextLine();
 
-    // volume calculations
-    final double volume = (4d / 3d) * Math.PI * Math.pow(radius, 3);
+    // try catch to make sure no strings are entered.
+    try {
+      // converts string to double.
+      final double radius = Double.parseDouble(stringRadius);
 
-    // prints volume to console
-    System.out.format("The volume of the sphere is %.3f cm", volume);
-    System.out.println();
+      // calculates the volume.
+      final double volume = (4d / 3d) * Math.PI * Math.pow(radius, 3);
 
-    // close scanner
+      // prints volume to console.
+      System.out.format("The volume of the sphere is %.3f cm", volume);
+      System.out.println();
+
+    } catch (NumberFormatException error) {
+      System.out.println("Invalid input, please enter a number.");
+    }
+
+    // closes scanner.
     sc.close();
   }
 }
